f7cd508b88768508fae6abdf216d7a5715a7aa7c
ajrobrigado23 <albertrobrigado.ict@gmail.com>
2025-05-13 15:51:07 +0800

    Add files via upload
    Task 7 Java NC-III

---
+++ Task7.java
@@ -0,0 +1,26 @@
+public class Task7 {
+
+    public static void main(String[] args) {
+
+        System.out.println(addition(2, 2));
+        System.out.println(subtraction(2, 2));
+        System.out.println(multiplication(2, 2));
+        System.out.println(division(2, 2));
+    }
+
+    public static double addition(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiplication(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double division(double num1, double num2) {
+        return num1 / num2;
+    }
+}
